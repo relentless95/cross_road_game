373d0a2569b54dd4d86fe7e3daadd03f3fa28073
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-01-27 03:42:18 +0100

    added the positon of the player

---
+++ classes/player.js
@@ -0,0 +1,36 @@
+class Player {
+    constructor(width, height, color, x, y) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.x = x;
+        this.y = y;
+
+        this.speedX = 0;
+        this.speedY=0;
+    }
+
+    update() {
+        let ctx = myGameArea.context;
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+      }
+    
+      newPos() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+      }
+    
+      left() {
+        return this.x;
+      }
+      right() {
+        return this.x + this.width;
+      }
+      top() {
+        return this.y;
+      }
+      bottom() {
+        return this.y + this.height;
+      }
+}
